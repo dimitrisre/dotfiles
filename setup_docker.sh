aa71afe97781974a484af7ec05a3b576ab5ce2f5
Dimitris Tsolis <dimitris.re@gmail.com>
2018-11-20 20:06:09 +0200

    Fixing docker installation process

--- setup_docker.sh
+++ setup_docker.sh
@@ -6,13 +6,13 @@ function install_docker {
     curl \
     software-properties-common
 
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
   sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+     "deb [arch=amd64] https://download.docker.com/linux/debian \
+     $(lsb_release -cs) \
+     stable"
 
   sudo apt update
-  sudo apt-get install docker -y
+  sudo apt-get install docker-ce -y
 }
