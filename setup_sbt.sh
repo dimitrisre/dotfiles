d075dc6eef097148011d0ecade9fdf7710674695
Dimitris Tsolis <dimitris.re@gmail.com>
2018-11-20 15:39:49 +0200

    Fixing problem on versioning

--- setup_sbt.sh
+++ setup_sbt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function install_sbt_from_git {
   sbt_version=$(curl -L https://api.github.com/repos/sbt/sbt/releases/latest | jq .name -r)
-  wget "https://piccolo.link/sbt-$sbt_version.zip" -O /tmp/sbt-`echo $sbt_version`.zip && cd /tmp
+  wget "https://piccolo.link/sbt-$sbt_version.zip" -O /tmp/sbt-$(echo $sbt_version).zip && cd /tmp
   sudo unzip sbt-`echo $sbt_version`.zip /opt
   sudo ln -s /opt/sbt/bin/sbt /usr/bin/sbt
 }
--- setup_scala.sh
+++ setup_scala.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function install_scala {
   scala_version=$(curl -L https://api.github.com/repos/scala/scala/releases/latest | jq .tag_name -r)
-  wget "https://api.github.com/repos/scala/scala/zipball/$scala_version" -O /tmp/scala-`echo $sbt_version`.zip && cd /tmp
+  wget "https://api.github.com/repos/scala/scala/zipball/$scala_version" -O /tmp/scala-$(echo $scala_version).zip && cd /tmp
   sudo unzip scala-`echo $scala_version`.zip /opt
 }
