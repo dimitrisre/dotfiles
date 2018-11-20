c0b1e96b549455c19980ed4f043a695ba3ff3ec5
Dimitris Tsolis <dimitris.re@gmail.com>
2018-11-20 16:39:53 +0200

    Fixing error on echo command

--- setup_node.sh
+++ setup_node.sh
@@ -2,8 +2,8 @@
 function install_node {
   DISTRO=$(lsb_release -c -s)
   NODEREPO="node_11.x"
-  sudo echo 'deb https://deb.nodesource.com/$(echo $NODEREPO) $(echo $DISTRO) main' > /etc/apt/sources.list.d/nodesource.list
-  sudo echo 'deb-src https://deb.nodesource.com/$(echo $NODEREPO) $(echo $DISTRO) main' >> /etc/apt/sources.list.d/nodesource.list
+  sudo echo "deb https://deb.nodesource.com/$NODEREPO $DISTRO main" > /etc/apt/sources.list.d/nodesource.list
+  sudo echo "deb-src https://deb.nodesource.com/$NODEREPO $DISTRO main" >> /etc/apt/sources.list.d/nodesource.list
   sudo apt update
   sudo apt install nodejs -y
 }
