48fa8da49c98b061091e5dc148beb6d9832c3077
Dimitris Tsolis <dimitris.re@gmail.com>
2018-11-20 23:01:38 +0200

    Adding etcher setup script

---
+++ setup_etcher.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+function install_etcher {
+  pushd $HOME/my_configuration
+  etcher_version=$(curl --silent "https://api.github.com/repos/balena-io/etcher/releases/latest" | jq -r .tag_name)
+  short_version=${etcher_version:1:${#etcher_version}}
+  echo "Installing etcher version $etcher_version - ($short_version)"
+  if [ ! -d "$HOME/programms" ]; then
+    mkdir $HOME/programms
+  fi
+  cd $HOME/programms
+  wget "https://github.com/balena-io/etcher/releases/download/$etcher_version/balena-etcher-electron-$short_version-x86_64.AppImage" -O /tmp/etcher-$(echo $short_version).appimage
+  popd
+}
