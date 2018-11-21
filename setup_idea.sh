1ef3608ba8c1ca8d93f446552bace0a75bf7b82e
Dimitris Tsolis <dimitris.re@gmail.com>
2018-11-21 22:44:08 +0200

    Fixing error on sed path

--- setup_idea.sh
+++ setup_idea.sh
@@ -4,7 +4,8 @@ function install_idea {
   release_url="https://data.services.jetbrains.com/products/releases?code=IIC&latest=true"
   download_url=$(wget $release_url -q -O - | jq '.IIC[0].downloads.linux.link' -r)
   download_file="/tmp/ideaIC.tar.gz"
-  installation_dir="$HOME/opt/idea"
+  mkdir -p $HOME/programms
+  installation_dir="$HOME\/programms\/idea\/"
 
   wget $download_url -O $download_file
   mkdir -p $installation_dir
@@ -12,7 +13,7 @@ function install_idea {
   rm $download_file
 
   cp $HOME/dotfiles/desktop_files/jetbrains-idea-ce.desktop /tmp/
-  sed -i -e "s/TO_BE_REPLACED/$installation_dir/g" /tmp/jetbrains-idea-ce.desktop
+  sed -i -e 's|TO_BE_REPLACED|'$installation_dir'|g' /tmp/jetbrains-idea-ce.desktop
   my_desktop_file=/tmp/jetbrains-idea-ce.desktop
   cp $my_desktop_file $HOME/.local/share/applications/
   rm $my_desktop_file
