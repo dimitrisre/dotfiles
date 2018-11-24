fbb252c2d892005c34f79f8af0f4fad4171902d8
Dimitris Tsolis <dimitris.re@gmail.com>
2018-11-24 20:40:13 +0200

    Adding support for gnome extensions

--- load_gnome_settings.sh
+++ load_gnome_settings.sh
@@ -2,6 +2,7 @@
 function restore_gnome_settings {
   echo "____________ Loading gnome settings ____________"
   cp $HOME/dotfiles/config_files/gnome_settings.conf /tmp/
+  cp -R $HOME/dotfiles/desktop_files/extensions $HOME/.local/share/gnome-shell/
   sed -i -e "s/TO_BE_REPLACED/$USER/g" /tmp/gnome_settings.conf
   dconf load /org/gnome/ < /tmp/gnome_settings.conf
   rm /tmp/gnome_settings.conf
